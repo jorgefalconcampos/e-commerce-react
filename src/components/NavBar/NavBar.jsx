762668e496ba69e4dbd0a70bebe7b5103285f5eb
Jorge Falcón <jorgef_campos@outlook.com>
2022-07-03 23:13:36 -0500

    cambio de Nav.Link => NavLink

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,46 +5,59 @@ import CartWidget from "./CartWidget";
 import { BoxSeam } from "react-bootstrap-icons";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  
   const linkStyle = {
+    display: "block",
     textDecoration: "none",
-    
-  }
+  };
+
+  const linkStyleActive = {
+    textDecoration: "underline",
+    fontWeight: "bold",
+  };
+
+  const isLinkActive = (isActive) => {
+    if (isActive) {
+      return "nav-item-active";
+    } else {
+      return "nav-item";
+    }
+  };
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Link to="/" style={linkStyle}>
-          <Navbar.Brand href="#home">
-            <BoxSeam color="white" size={60} className="px-2" />
-            Mi tienda en React
-          </Navbar.Brand>
-        </Link>
+        <NavLink to="/" className="nav-item">
+          <BoxSeam color="white" size={60} className="px-2" />
+          Mi tienda en React
+        </NavLink>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Container fluid>
             <Row>
               <Col className="text-center">
-                <Nav className="me-auto">
-                  <Link to="/" style={linkStyle}>
-                    <Nav.Link href="#home">Inicio</Nav.Link>
-                  </Link>
-                  <Link to="/products" style={linkStyle}>
-                    <Nav.Link href="#products">Productos</Nav.Link>
-                  </Link>
-                  
+                <Nav className="me-auto my-auto">
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) => isLinkActive(isActive)}
+                  >
+                    Inicio
+                  </NavLink>
+                  <NavLink
+                    to="/products"
+                    className={({ isActive }) => isLinkActive(isActive)}
+                  >
+                    Productos
+                  </NavLink>
                 </Nav>
               </Col>
               <Col lg="auto" className="text-center">
                 <Nav class="me-auto">
-                  <Link to="/cart" style={linkStyle}>
-                    <Nav.Link href="#cart">
-                      <CartWidget />
-                    </Nav.Link>
-                  </Link>
+                  <NavLink to="/cart" className="nav-item">
+                    <CartWidget />
+                  </NavLink>
                 </Nav>
               </Col>
             </Row>
